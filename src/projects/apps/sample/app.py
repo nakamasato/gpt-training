f4df83c6db1004357fa3c1d04e2bc92fd90e95e5
Masato Naka <masatonaka1989@gmail.com>
2024-02-04 15:01:28 +0900

    add tests

--- src/projects/apps/sample/app.py
+++ src/projects/apps/sample/app.py
@@ -4,39 +4,45 @@ from langchain.memory import ConversationBufferMemory
 from langchain_community.callbacks import StreamlitCallbackHandler
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables.config import RunnableConfig
-from langchain_openai import OpenAI
 
 from src.examples.agent_react import CUSTOM_PROMPT, tools
+from src.libs import get_llm
 
-st.title("Langchain Sample App")
-st.info("こちらは掛け算のツールが使えるAgentとの対話を行うサンプルアプリです。")
 
-llm = OpenAI(temperature=0, streaming=True)
-prompt = PromptTemplate.from_template(template=CUSTOM_PROMPT)
+def main(llm):
+    st.title("Langchain Sample App")
+    st.info("こちらは掛け算のツールが使えるAgentとの対話を行うサンプルアプリです。")
 
-agent = create_react_agent(
-    llm=llm,
-    tools=[tools[0]],
-    prompt=prompt,
-)
+    prompt = PromptTemplate.from_template(template=CUSTOM_PROMPT)
 
-memory = ConversationBufferMemory(
-    memory_key="chat_history",
-    return_messages=True,
-)
+    agent = create_react_agent(
+        llm=llm,
+        tools=[tools[0]],
+        prompt=prompt,
+    )
 
-agent_executor = AgentExecutor(
-    agent=agent,
-    tools=tools,
-    memory=memory,
-    verbose=True,
-    handle_parsing_errors=False,
-)
-prompt = st.chat_input()
-if prompt:
-    st.chat_message("user").write(prompt)
-    with st.chat_message("assistant"):
-        st_callback = StreamlitCallbackHandler(st.container())
-        config = RunnableConfig(callbacks=[st_callback])
-        response = agent_executor.invoke({"input": prompt}, config=config)
-        st.write(response["output"])
+    memory = ConversationBufferMemory(
+        memory_key="chat_history",
+        return_messages=True,
+    )
+
+    agent_executor = AgentExecutor(
+        agent=agent,
+        tools=tools,
+        memory=memory,
+        verbose=True,
+        handle_parsing_errors=False,
+    )
+    prompt = st.chat_input()
+    if prompt:
+        st.chat_message("user").write(prompt)
+        with st.chat_message("assistant"):
+            st_callback = StreamlitCallbackHandler(st.container())
+            config = RunnableConfig(callbacks=[st_callback])
+            response = agent_executor.invoke({"input": prompt}, config=config)
+            st.write(response["output"])
+
+
+if __name__ == "__main__":
+    llm = get_llm()
+    main(llm)
