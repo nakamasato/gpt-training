7159bdf795e435dd00569c5bee6088134d43feba
Naka Masato <masatonaka1989@gmail.com>
2024-02-10 09:40:16 +0800

    chore: upgrade python to 3.12 (#23)

--- tests/projects/app/streamlit/test_app.py
+++ tests/projects/app/streamlit/test_app.py
@@ -2,7 +2,7 @@ from streamlit.testing.v1 import AppTest
 
 
 def test_app_user_summary_home():
-    at = AppTest.from_file("src/projects/apps/sample/app.py")
+    at = AppTest.from_file("src/projects/apps/streamlit/app.py")
     at.run()
     assert not at.exception
     assert at.title[0].value == "Langchain Sample App"
